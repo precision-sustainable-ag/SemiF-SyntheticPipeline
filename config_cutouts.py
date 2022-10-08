959d060133bf59a547fb22b93f29c03c4642e49b
weedsci <mattkutugata@gmail.com>
2022-10-07 21:20:29 -0500

    minor

--- config_cutouts.py
+++ config_cutouts.py
@@ -1,16 +1,10 @@
-import logging
-from pathlib import Path
-
-import numpy as np
 from omegaconf import DictConfig
 
 
 def main(cfg: DictConfig) -> None:
     if cfg.cutouts.mode == "balanced":
+        # TODO configure "species" and "balanced" to work
         print("balanced")
-    path = "/home/weedsci/matt/SemiF-AnnotationPipeline/data/semifield-cutouts/MD_2022-07-05/MD_Row-10_1657032952_6.json"
-    cutoutdir = "/home/weedsci/matt/SemiF-AnnotationPipeline/data/semifield-cutouts"
-    batch_id = "MD_2022-07-06"
-    csv_path = "MD_2022-07-06.csv"
+
     # cutoutmeta2csv(cutoutdir, batch_id, csv_path)
     pass
