c24736637038f51343d3870a55e8452094319308
HaydenMcCleary <haden0pm@gmail.com>
2025-06-12 14:03:55 -0400

    restore more files, going to move to a query approach for the resizing of cutouts

--- main.py
+++ main.py
@@ -16,25 +16,15 @@ log = logging.getLogger(__name__)
 def run_SYNTH(cfg: DictConfig) -> None:
     cfg = OmegaConf.create(cfg)
     log.info(f"Starting task {','.join(cfg.tasks)}")
+    
+    for tsk in cfg.tasks:
+        try:
+            task = get_method(f"{tsk}.main")
+            task(cfg)
 
-    resize_area = None
-
-    try:
-        if "create_recipes" in cfg.tasks:
-            create_fn = get_method("create_recipes.main")
-            resize_area = create_fn(cfg)
-
-        if "move_cutouts" in cfg.tasks:
-            move_fn = get_method("move_cutouts.main")
-            move_fn(cfg)
-
-        if "synthesize" in cfg.tasks:
-            synth_fn = get_method("synthesize.main")
-            synth_fn(cfg, resize_area=resize_area)
-
-    except Exception as e:
-        log.exception("Failed")
-        return
+        except Exception as e:
+            log.exception("Failed")
+            return
 
 
 if __name__ == "__main__":
