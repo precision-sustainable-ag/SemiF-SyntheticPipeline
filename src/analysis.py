096ef8bd9d81d23c61811dc21c0118986d7932c9
MathewAaron <aaron.mathew0217@gmail.com>
2025-06-18 14:59:11 -0400

    fixing pdf name bug, adding check for no analysis tasks

--- src/analysis.py
+++ src/analysis.py
@@ -22,21 +22,31 @@ def main(cfg: DictConfig) -> None:
     log.info("Reached analysis.py")
     log.info("Going through subtasks under analysis")
 
-    for sub_task_name in analysis_subtasks:
+    if cfg.tasks.analysis : 
+        for sub_task_name in analysis_subtasks:
 
-        if sub_task_name in TASK_REGISTRY:
-            log.info(f"Running task {sub_task_name}")
-            try:
-                TASK_REGISTRY[sub_task_name](cfg)
-            except Exception as e:
-                log.error(f"Error running task {sub_task_name}: {e}")
-                raise
+            if sub_task_name in TASK_REGISTRY:
+                log.info(f"Running task {sub_task_name}")
+                try:
+                    TASK_REGISTRY[sub_task_name](cfg)
+                except Exception as e:
+                    log.error(f"Error running task {sub_task_name}: {e}")
+                    raise
 
-        else:
-            log.error(f"Task {sub_task_name} not found in analysis task registry")
-            raise ValueError(f"Task {sub_task_name} not found in analysis task registry")
+            else:
+                log.error(f"Task {sub_task_name} not found in analysis task registry")
+                raise ValueError(f"Task {sub_task_name} not found in analysis task registry")
     
-    log.info("Analysis completed.")
+        log.info("Analysis completed.")
+    else : 
+        # Description
+        task_list = list(set(name.lower() for name in TASK_REGISTRY.keys()))
+        if len(task_list) > 1:
+            titled = [s.title() for s in task_list]
+            task_str = ', '.join(titled[:-1]) + f", and {titled[-1]}"
+        else:
+            task_str = task_list[0].title()
+        log.info(f"No Analysis tasks requested.. set a task [{task_str}] in config.yaml")
     return
 
 if __name__ == "__main__":
