2ed8dcf1ca13b90806805d31dcf8eafdc1061a1f
HaydenMcCleary <haden0pm@gmail.com>
2025-06-24 17:07:28 -0400

    refactoring is needed for add graphs function of pdf.py

--- src/analyze/analyze_preprocessed_cutouts.py
+++ src/analyze/analyze_preprocessed_cutouts.py
@@ -15,6 +15,31 @@ class PreprocessAnalyzer():
 
 
 def main(cfg: DictConfig) -> None:
-    print("hello world")
+
+    # # Total num of cutouts
+    # num_cutouts = all_cutouts.num_cutouts, specified_cutouts.num_cutouts
+
+    # Create PDF from graphs
+    report = PDFDrafter(cfg, (0,0))
+
+    # Heading
+    heading = "Analysis of Preprocessing"
+
+    # Description
+    preprocesses_list = list(set(preprocess.lower().replace('_', ' ') for preprocess in cfg.preprocess_cutouts.keys()))
+    sorted_preprocesses = sorted(preprocesses_list, key=lambda s: s.lower())
+    if len(sorted_preprocesses) > 1:
+        titled = [s.title() for s in sorted_preprocesses]
+        species_str = ', '.join(titled[:-1]) + f", and {titled[-1]}"
+    else:
+        species_str = sorted_preprocesses[0].title()
+        
+    technique_or_techniques = "techniques" if (len(sorted_preprocesses) > 1) else "technique"
+    description = f"The following is a report of the {species_str} preprocessing {technique_or_techniques}. The aim is to display the cutouts before and after preprocessing, allowing the user to see the effects the preprocessing has on the cuttous."
+
+    report.initialize_heading_and_description(heading, description)
+    # report.add_graphs_to_pdf()
+    report.save_pdf()
+
 
     report = PDFDrafter(cfg, )
